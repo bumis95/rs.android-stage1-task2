3f53d1ac117dd05ce8ee5e3fbfe5718c4dac2121
bumis95 <bumis95@gmail.com>
2020-04-09 12:23:27 +0300

    add Fibonacci

--- src/main/kotlin/subtask6/Fibonacci.kt
+++ src/main/kotlin/subtask6/Fibonacci.kt
@@ -2,8 +2,25 @@ package subtask6
 
 class Fibonacci {
 
-    // TODO: Complete the following function
     fun productFibonacciSequenceFor(n: Int): IntArray {
-        throw NotImplementedError("Not implemented")
+
+        val result = IntArray(3)
+        var previous = 0
+        var next = 1
+
+        while (previous * next <= n) {
+            if(previous * next == n) return result.apply {
+                set(0, previous)
+                set(1, next)
+                set(2, 1)
+            }
+            next += previous
+            previous = next - previous
+        }
+        return result.apply {
+            set(0, previous)
+            set(1, next)
+            set(2, 0)
+        }
     }
 }
