bb10063d61ee69675e90c838dd5b4788ba8fca04
bumis95 <bumis95@gmail.com>
2020-04-09 07:37:28 +0300

    add DateFormatter

--- src/main/kotlin/subtask1/DateFormatter.kt
+++ src/main/kotlin/subtask1/DateFormatter.kt
@@ -1,9 +1,39 @@
 package subtask1
 
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateFormatter {
 
-    // TODO: Complete the following function
     fun toTextDay(day: String, month: String, year: String): String {
-        throw NotImplementedError("Not implemented")
+
+        val dateToValidate = "$day.$month.$year"
+
+        val sdf = SimpleDateFormat("dd.MM.yyyy")
+        sdf.isLenient = false
+        val date: Date
+        try {
+            date = sdf.parse(dateToValidate)
+        } catch (e: ParseException) {
+            return "Такого дня не существует"
+        }
+
+        return "$day ${chooseMonth(month)}, ${SimpleDateFormat("EEEE").format(date)}"
+    }
+    
+    fun chooseMonth(month: String) = when(month.toInt()) {
+        1 -> "января"
+        2 -> "февраля"
+        3 -> "марта"
+        4 -> "апреля"
+        5 -> "мая"
+        6 -> "июня"
+        7 -> "июля"
+        8 -> "августа"
+        9 -> "сентября"
+        10 -> "октября"
+        11 -> "ноября"
+        else -> "декабря"
     }
 }
