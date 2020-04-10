1ba36a1029a09adb94587dcb4b2963e8722c8db5
bumis95 <bumis95@gmail.com>
2020-04-10 12:56:44 +0300

    add Blocks

--- src/main/kotlin/subtask5/Blocks.kt
+++ src/main/kotlin/subtask5/Blocks.kt
@@ -1,11 +1,35 @@
 package subtask5
 
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import kotlin.reflect.KClass
 
 class Blocks {
 
-    // TODO: Complete the following function
-    fun getData(blockA: Array<Any>, blockB: KClass<*>): Any {
-        throw NotImplementedError("Not implemented")
+    fun getData(blockA: Array<*>, blockB: KClass<*>): Any {
+
+        val stringList: List<String>
+        val intList: List<Int>
+        val dateList: List<LocalDate>
+
+        when(blockB) {
+            String::class -> {
+                stringList = blockA.filterIsInstance<String>()
+                var result = ""
+                stringList.forEach { result += it }
+                return result
+            }
+            Int::class -> {
+                intList = blockA.filterIsInstance<Int>()
+                var result = 0
+                intList.forEach { result += it }
+                return result
+            }
+            else -> {
+                dateList = blockA.filterIsInstance<LocalDate>()
+                val max = dateList.max()?.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+                return max!!
+            }
+        }
     }
 }
